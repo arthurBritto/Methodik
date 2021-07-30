9f5d63f541209255ed96f7d06cf638c7ada36fa2
ArthurBritto <arthurbrittos@gmail.com>
2021-07-30 17:54:11 +0200

    MTH-2 - Refactor, change color on preferences flags

--- app/src/main/java/com/arthurbritto/methodik/view/PanelAdapter.java
+++ app/src/main/java/com/arthurbritto/methodik/view/PanelAdapter.java
@@ -1,12 +1,14 @@
 package com.arthurbritto.methodik.view;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
+import androidx.core.widget.ImageViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.arthurbritto.methodik.R;
